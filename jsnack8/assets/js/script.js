adec0c6f8bd0866b13c355502e7e31e4be978696
agostinoluca <agostino.luca89@gmail.com>
2023-11-29 19:55:53 +0100

    start exercise jsnack8

--- jsnack8/assets/js/script.js
+++ jsnack8/assets/js/script.js
@@ -2,3 +2,7 @@
 Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero.
 */
+
+// chiedo un numero di quattro cifre all'utente con un  prompt inserendolo in una costante
+
+// chiedo a javascript di sommare i singoli numeri contenuti nella cifra inserita
