3accc89e9cadaea67e83d5710633ecadbaa3f1c2
agostinoluca <agostino.luca89@gmail.com>
2023-11-29 19:22:41 +0100

    start exercise jsnack5

--- jsnack5/assets/js/script.js
+++ jsnack5/assets/js/script.js
@@ -3,3 +3,11 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
+
+// creo un array vuoto
+
+// creo un ciclo con un prompt
+
+// creo una condizione per stabilire quali numeri verranno salvati nella mia array
+
+// stampo in pagina la mia array
