86d28c9551480f99edea4c15b3bc3e923b389262
agostinoluca <agostino.luca89@gmail.com>
2023-11-29 19:49:50 +0100

    end exercise jsnack5

--- jsnack5/assets/js/script.js
+++ jsnack5/assets/js/script.js
@@ -15,11 +15,11 @@ for (let i = 0; i < 6; i++) {
 
 // creo una condizione per stabilire quali numeri verranno salvati nella mia array
 if (numberChoosen % 2 !== 0) {
+    // se, dividendo per 2, restituisce un resto diverso da 0, viene inserito nella array
     numberUser.push(numberChoosen);
 }
 
-// stampo in pagina la mia array
-
-console.log(numberUser);
-
+// stampo in pagina la array
+const oddNumbers = document.querySelector('.number_choosen');
+oddNumbers.innerHTML = (`Hai inserito i seguenti numeri dispari:<br>` + numberUser);
 }
