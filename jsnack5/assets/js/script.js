2111f7f0a585f83589e4ccb60979134d0d720aeb
agostinoluca <agostino.luca89@gmail.com>
2023-11-29 19:26:08 +0100

    add for with prompt

--- jsnack5/assets/js/script.js
+++ jsnack5/assets/js/script.js
@@ -5,8 +5,13 @@ se è dispari inseriscilo nell’array.
 */
 
 // creo un array vuoto
+const numberUser = [];
 
 // creo un ciclo con un prompt
+for (let i = 0; i < 6; i++) {
+    const numberChoose = prompt('Scegli un numero intero');
+    console.log(numberChoose);    
+}
 
 // creo una condizione per stabilire quali numeri verranno salvati nella mia array
 
