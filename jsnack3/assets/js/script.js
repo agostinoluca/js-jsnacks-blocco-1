851c806d7d231bebe227de68e201e1fe85491e66
agostinoluca <agostino.luca89@gmail.com>
2023-12-14 19:26:48 +0100

    start jsnack3

---
+++ jsnack3/assets/js/script.js
@@ -0,0 +1,63 @@
+/* 
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: ‘leone’, famiglia: ‘felidi’, classe: ‘mammiferi’ },
+  { nome: ‘cane’, famiglia: ‘canidi’, classe: ‘mammiferi’ },
+  { nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ },
+]
+Crea un nuovo array con la lista dei mammiferi.
+*/
+
+console.log('Hello World');
+
+// creo un'array di oggetti
+const animalsList = [
+    { 
+        nome: 'leone', 
+        famiglia: 'felidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'cane', 
+        famiglia: 'canidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'gallina', 
+        famiglia: 'fasianidi', 
+        classe: 'uccelli' 
+    },
+    {
+        nome: 'elefante', 
+        famiglia: 'elefantidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'gatto', 
+        famiglia: 'felidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'cavallo', 
+        famiglia: 'equidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'aquila', 
+        famiglia: 'accipitridi', 
+        classe: 'uccelli' 
+    },
+    { 
+        nome: 'tigre', 
+        famiglia: 'felidi', 
+        classe: 'mammiferi' 
+    }
+  ]
+
+// con un ciclo itero la lista
+
+// con una condizione seleziono gli oggetti che come classe hanno "mammiferi"
+
+// pusho dentro una nuova lista gli oggetti selezionati
